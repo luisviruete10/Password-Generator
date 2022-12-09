14c80acfb041788070cf6809bd708c4a1a2de6cb
leviruete@hotmail.com <leviruete@hotmail.com>
2022-12-08 21:34:36 -0800

    started the js

--- password/script.js
+++ password/script.js
@@ -1,14 +1,35 @@
 // Assignment Code
+function writePassword() 
+var passwordText = document.querySelector("#password");
+
+passwordText.addEventListener('keydown', function (event) {
+
+  var key = event.key();
+  var alphabetNumbers = 'abcdefghijklmnopqrstuvwxyz0123456789 '.split(
+    ''
+  );
+  if (alphabetNumbers.includes(key)) {
+    for (var i = 0; i < passwordText.length; i++) {
+      passwordText[i].textContent += event.key;
+    }
+  }
+});
+
+
+// Write password to the #password input;
+  
+
+// pass
+
+// // Add event listener to generate button
 var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
+
+// var passwordText = document.querySelector("#password");
+// function writePassword() {
+//   var password = generatePassword();
+// }
