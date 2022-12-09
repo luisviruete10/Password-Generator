e140517233aa27e83e5a76d1a894207e52de910e
leviruete@hotmail.com <leviruete@hotmail.com>
2022-12-08 23:04:43 -0800

    got the code going and updated README.md

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Password-Generator
+In this challenge I had to build a password generator.I left ssome notes about how of the new stuff I leanered works. I couldn't figure how to put all of the characters in the same array, so i made different ones for each. Some stuff i had to get answers from Google , Youtube , Reddit and MDN to understand where to eve begin with this project.
+
+I think i got all of the Acceptance Criteria that was asked for. I did change the css a little to give it my own touch.
--- password/script.js
+++ password/script.js
@@ -1,35 +1,78 @@
+// Our password character length
+var characterLength = 128;
+var choiceArr = [];
+// All the characters we can chose from
+var specialCharArr = ["!", "@", "#", "%", "^", "&", "*", "(", ")", "[", "]", "{", "}", "|", "?", "/", ",", ";", ".", "<", ">", ".", ":"];
+var lowerCaseArr = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCaseArr = [ "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numberArr = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", ];
+
+
 // Assignment Code
-function writePassword() 
-var passwordText = document.querySelector("#password");
-
-passwordText.addEventListener('keydown', function (event) {
-
-  var key = event.key();
-  var alphabetNumbers = 'abcdefghijklmnopqrstuvwxyz0123456789 '.split(
-    ''
-  );
-  if (alphabetNumbers.includes(key)) {
-    for (var i = 0; i < passwordText.length; i++) {
-      passwordText[i].textContent += event.key;
-    }
-  }
-});
-
-
-// Write password to the #password input;
-  
-
-// pass
-
-// // Add event listener to generate button
 var generateBtn = document.querySelector("#generate");
+
+
+// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 
+// Write password to the #password input;
+function writePassword() {
+  var correctPrompts = getPrompts();
+var passwordText = document.querySelector("#password");
+
+  if (correctPrompts) {
+    var newPassword = generatePassword();
+    passwordText.value = newPassword ;
+  }
+  passwordText.value = "No Good";
+  
+}
+
+// I would generatePassword based on the prompts
+function generatePassword(){
+var password = " ";
+for(var i = 0; i < characterLength; i++) {
+  var randomIndex = Math.floor(Math.random() * choiceArr.length);
+  password = password + choiceArr[randomIndex];
+    }
+    return password;
+
+}
+
+
+function getPrompts(){
+
+  choiceArr = [];
+
+  characterLength = parseInt(prompt("How many characters do you want your password to be? (8-128 charactwers"));
+
+  // isNaN lets use characters that are not numbers as numbers
+  if(isNaN(characterLength)|| characterLength < 8 || characterLength > 128){
+
+    // if they chose lower than 8 or more than 128 this messege below will appear
+    alert("Character length has to a number, 8 -128 digits. Please try again.");
+    return false;
+  }
+  if (confirm("Would you like lowercase letters in your password?")) {
+    choiceArr = choiceArr.concat(lowerCaseArr);
+  }
+
+  if (confirm("Would you like uppercase letters in your password?")) {
+    choiceArr = choiceArr.concat(upperCaseArr);
+  }
+
+  if (confirm("Would you like special characters in your password?")) {
+    choiceArr = choiceArr.concat(specialCharArr);
+  }
+
+  if (confirm("Would you like numbers in your password?")) {
+    choiceArr = choiceArr.concat(numberArr);
+  }
+  return true;
+
+}
 
 
 
-// var passwordText = document.querySelector("#password");
-// function writePassword() {
-//   var password = generatePassword();
-// }
+
